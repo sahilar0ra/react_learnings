358e7bb8f1b2811836114e5e13c06d7fb74c65c9
Sahil Arora <sahilaroraesl@gmail.com>
2025-01-15 22:33:20 +0530

    learning about component based architecture

---
+++ class-2/vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return <div>
+    {/* <h1>Hello from Vite + React</h1> */}
+    <Navbar></Navbar>
+   </div>;
+  
+}
+
+export default App
---
+++ class-2/vite-project/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+
+
+function Navbar() {
+    return (
+    <div>
+        <h1>I am a Nav bar</h1>
+    </div>
+    );
+}
+
+export default Navbar
